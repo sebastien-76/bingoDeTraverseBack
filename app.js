56c1011f923208dc83fd5a8f157cba8c8a676e1d
sebastien <sebastien.divers@gmail.com>
2024-07-10 14:59:35 +0200

    SMA/feat/creation branche develop

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 /* Récupération du paquet express */
 const express = require('express')
 
+/* Importer le paquet sequelize */
+const sequelize = require('./src/DB/sequelize')
+
+
 /* Creer une instance de l'application express  (serveur web) */
 const app = express()
 
 /* Middleware pour parser le corps de la requête */
 app.use(express.json())
+
+/* Initialisation de la base de données */
+sequelize.initDb()
 
 /* Importer les routes */
 
