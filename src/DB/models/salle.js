b903de32cd09973c37a6df08f806d51f8d46da8a
EmmaCremeaux <122892031+EmmaCremeaux@users.noreply.github.com>
2024-07-16 17:12:07 +0200

    ECRE/fix/changement nom en name

--- src/DB/models/salle.js
+++ src/DB/models/salle.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true
         },
-        nom: {
+        name: {
             type: DataTypes.STRING
         }
     })
