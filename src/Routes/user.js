933b97821d04d4a9abd67e84122bbfec967e3490
sebastien <sebastien.divers@gmail.com>
2024-08-27 17:37:39 +0200

    SMA/fix/auth

--- src/Routes/user.js
+++ src/Routes/user.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../Middleware/auth/auth');
+
 const multer = require('../Middleware/multer-config');
 
 const userController = require('../controllers/user');
 
 router.get('/', userController.getUsers);
-router.get('/:id', userController.getUser);
+router.get('/:id', auth,  userController.getUser);
 router.post('/', multer, userController.signUp);
-router.put('/:id', multer, userController.updateUser);
+router.put('/:id', auth, multer, userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router
