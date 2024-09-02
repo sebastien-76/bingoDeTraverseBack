88f38c1f9d0455636c0aa814d07e793638ce720b
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-09-02 10:50:49 +0200

    SMA/feat/grilleAuth

--- src/Routes/grille.js
+++ src/Routes/grille.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../Middleware/auth/auth');
 const grilleController = require('../controllers/grille');
 
 router.post('/user/:userId', grilleController.createGrille);
-router.get('/user/:userId', grilleController.getGrilleByUser);
-router.get('/:id', grilleController.getGrilleById);
-router.put('/:id', grilleController.updateGrille);
+router.get('/user/:userId',auth,  grilleController.getGrilleByUser);
+router.get('/:id',auth,  grilleController.getGrilleById);
+router.put('/:id',auth,  grilleController.updateGrille);
 
 
 module.exports = router;
