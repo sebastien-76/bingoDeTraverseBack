199b9a0b8e46e34acc70e6112acb1bb8940f52e8
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-09-12 10:46:22 +0200

    SMA/fix/correction auth salles

--- src/Routes/salle.js
+++ src/Routes/salle.js
@@ -5,7 +5,7 @@ const auth = require('../Middleware/auth/auth');
 
 const salleController = require('../controllers/salle');
 
-router.get('/', auth, salleController.getSalles);
+router.get('/', salleController.getSalles);
 router.get('/:id', auth, salleController.getSalle);
 router.post('/', auth, salleController.createSalle);
 router.put('/:id', auth, salleController.updateSalle);
