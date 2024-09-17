cb590510c3cef5f936b9fb92e2d2d1aafb2c286e
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-09-17 14:01:35 +0200

    SMA/fix/gestion taille images

--- src/Middleware/multer-config.js
+++ src/Middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const SharpMulter = require("sharp-multer");
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -6,21 +7,31 @@ const MIME_TYPES = {
     'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
+const storage =
+    SharpMulter({
+        destination: (req, file, callback) => callback(null, "images"),
+        imageOptions: {
+            fileFormat: "png",
+            quality: 80,
+            resize: { width: 500, height: 500 },
+        }
+    });
+
+/* multer.diskStorage({
     destination: (req, file, callback) => {
-        /* Définition du dossier dans lequel sont stokés les fichiers */
+        //Définition du dossier dans lequel sont stokés les fichiers
         callback(null, 'images');
     },
     filename: (req, file, callback) => {
-        /* Création du nom du fichier en remplacant les espaces par des underscores */
+        //Création du nom du fichier en remplacant les espaces par des underscores
         const name = file.originalname.split(' ').join('_');
 
-        /* Création de l'extension du fichier */
+        //Création de l'extension du fichier
         const extension = MIME_TYPES[file.mimetype];
 
-        /* Création du nom du fichier comprenant le name, un timestamp et l'extension */
+        //Création du nom du fichier comprenant le name, un timestamp et l'extension
         callback(null, name + Date.now() + '.' + extension);
     }
-});
+}); */
 
 module.exports = multer({ storage }).single('avatar');
