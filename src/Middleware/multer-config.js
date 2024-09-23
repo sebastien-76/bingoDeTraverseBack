faff1c9e6c8d95d5ac3638518e8e5abe7d2c216e
Sebastien MAILLET <sebastien.maillet@gmail.com>
2024-09-23 15:15:35 +0200

    SMA/fix/modif config multer

--- src/Middleware/multer-config.js
+++ src/Middleware/multer-config.js
@@ -1,12 +1,6 @@
 const multer = require('multer');
 const SharpMulter = require("sharp-multer");
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png'
-};
-
 const storage =
     SharpMulter({
         destination: (req, file, callback) => callback(null, "images"),
